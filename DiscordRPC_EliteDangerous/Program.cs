aa9cf0bf1314df73e6a7bf1021ecdf068f623a18
Blattoid <springtrap.bite87@gmail.com>
2020-08-20 18:45:06 +0100

    Adds functionality to start and stop when game is open and closed

--- DiscordRPC_EliteDangerous/Program.cs
+++ DiscordRPC_EliteDangerous/Program.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 using Somfic.Logging.Console;
 using Somfic.Logging.Console.Themes;
 using DiscordRPC;
+using System.Linq;
+using System.Threading;
 
 namespace DiscordRPC_EliteDangerous
 {
@@ -21,7 +23,7 @@ namespace DiscordRPC_EliteDangerous
         private static async Task Main(string[] args)
         {
             //Start Discord Rich Presence
-            discord = new DiscordRichPresence("716464991326306355");
+            discord = new DiscordRichPresence("746041178603913227");
 
             IHost host = Host.CreateDefaultBuilder()
                 .ConfigureLogging((context, logger) =>
@@ -42,11 +44,16 @@ namespace DiscordRPC_EliteDangerous
             EventHandlers.Exploration exploration = new EventHandlers.Exploration();
             api.Events.FSDJumpEvent += exploration.FSDJumpEvent;
 
+            //Start API
             await api.StartAsync();
 
-            //await Task.Delay(-1);
+            while (true)
+            {
+                Thread.Sleep(1500);
+                if (!discord.Online) continue; //wait for a connection
+                discord.Update();
+            }
         }
-
 
         //Will contain all event handlers, nested classes used for organisation
         class EventHandlers
@@ -55,8 +62,9 @@ namespace DiscordRPC_EliteDangerous
             {
                 public void FSDJumpEvent(object sender, FSDJumpEvent e)
                 {
+                    //Occurs when a jump concludes
                     Debug.WriteLine(string.Format("FSD JUMP EVENT!!!! {0}, {1}, {2}, {3}", e.Event, e.Body, e.BoostUsed, e.JumpDist));
-                    Program.discord.PresenceBottomText = "fsd_jump";
+                    discord.PresenceBottomText = DateTime.Compare(e.Timestamp, DateTime.Now).ToString(); //debug - test if the event is stale or recent
                 }
             }
         }
@@ -70,32 +78,42 @@ namespace DiscordRPC_EliteDangerous
         public DiscordRichPresence(string AppID)
         {
             rpc = new DiscordRpcClient(AppID);
-            rpc.Initialize();
+            TurnOn();
         }
-        public void Stop()
+        public void TurnOn()
         {
+            if (Online) return;
+            rpc.Initialize();
+            Update();
+            Online = true;
+        }
+        public void TurnOff()
+        {
+            if (!Online) return;
             rpc.ClearPresence();
             rpc.Deinitialize();
-            rpc.Dispose();
+            Online = false;
         }
+
+        public bool Online=false;
 
         //Presence text, updated from API events. Once the API supports reading the current player state on demand, we can replace default values with actual information in the constructor.
         public string PresenceTopText = "Unknown location"; //Should contain the star system name
         public string PresenceBottomText = "Unknown condition"; //Current action (docking, supercruise, jumping, fighting, using DSS, etc)
 
-        //Called after an API event updates our state, refreshes the Rich Presence with new information
+        //Called periodically to refresh the presence by the main loop
         public void Update()
         {
             RichPresence presence = new RichPresence
             {
-                Details = "details",
-                State = "state",
+                Details = PresenceTopText,
+                State = PresenceBottomText,
                 Assets = new Assets()
                 {
-                    LargeImageKey = "largeimg",
-                    LargeImageText = "largeimage_text",
-                    SmallImageKey = "smallimg",
-                    SmallImageText = "smallimage_text"
+                    LargeImageKey = "edlogo",
+                    LargeImageText = "Fly Dangerously!",
+                    SmallImageKey = "edlogo",
+                    SmallImageText = ""
                 }
             };
             rpc.SetPresence(presence);
