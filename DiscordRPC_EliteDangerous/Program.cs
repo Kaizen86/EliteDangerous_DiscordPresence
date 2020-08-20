37cfaa3b7254984cbb0cf0c4ee25b4210d157bb8
Blattoid <springtrap.bite87@gmail.com>
2020-08-20 17:08:14 +0100

    Adds foundations for Rich Presence

--- DiscordRPC_EliteDangerous/Program.cs
+++ DiscordRPC_EliteDangerous/Program.cs
@@ -8,15 +8,21 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Somfic.Logging.Console;
 using Somfic.Logging.Console.Themes;
+using DiscordRPC;
 
 namespace DiscordRPC_EliteDangerous
 {
     class Program
     {
         static EliteDangerousAPI api;
+        static DiscordRichPresence discord;
 
+        //Adapted from sample code
         private static async Task Main(string[] args)
         {
+            //Start Discord Rich Presence
+            discord = new DiscordRichPresence("716464991326306355");
+
             IHost host = Host.CreateDefaultBuilder()
                 .ConfigureLogging((context, logger) =>
                 {
@@ -32,7 +38,7 @@ namespace DiscordRPC_EliteDangerous
 
             api = ActivatorUtilities.CreateInstance<EliteDangerousAPI>(host.Services);
 
-            //Add events
+            //Subscribe event handlers to API
             EventHandlers.Exploration exploration = new EventHandlers.Exploration();
             api.Events.FSDJumpEvent += exploration.FSDJumpEvent;
 
@@ -40,16 +46,59 @@ namespace DiscordRPC_EliteDangerous
 
             //await Task.Delay(-1);
         }
+
+
+        //Will contain all event handlers, nested classes used for organisation
+        class EventHandlers
+        {
+            public class Exploration
+            {
+                public void FSDJumpEvent(object sender, FSDJumpEvent e)
+                {
+                    Debug.WriteLine(string.Format("FSD JUMP EVENT!!!! {0}, {1}, {2}, {3}", e.Event, e.Body, e.BoostUsed, e.JumpDist));
+                    Program.discord.PresenceBottomText = "fsd_jump";
+                }
+            }
+        }
     }
 
-    class EventHandlers
+    //Responsible for Discord RPC stuff
+    class DiscordRichPresence
     {
-        public class Exploration
+        private DiscordRpcClient rpc;
+        //Start/stop the client
+        public DiscordRichPresence(string AppID)
         {
-            public void FSDJumpEvent(object sender, FSDJumpEvent e)
+            rpc = new DiscordRpcClient(AppID);
+            rpc.Initialize();
+        }
+        public void Stop()
+        {
+            rpc.ClearPresence();
+            rpc.Deinitialize();
+            rpc.Dispose();
+        }
+
+        //Presence text, updated from API events. Once the API supports reading the current player state on demand, we can replace default values with actual information in the constructor.
+        public string PresenceTopText = "Unknown location"; //Should contain the star system name
+        public string PresenceBottomText = "Unknown condition"; //Current action (docking, supercruise, jumping, fighting, using DSS, etc)
+
+        //Called after an API event updates our state, refreshes the Rich Presence with new information
+        public void Update()
+        {
+            RichPresence presence = new RichPresence
             {
-                Debug.WriteLine(string.Format("FSD JUMP EVENT!!!! {0}, {1}, {2}, {3}", e.Event, e.Body, e.BoostUsed, e.JumpDist));
-            }
+                Details = "details",
+                State = "state",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "largeimg",
+                    LargeImageText = "largeimage_text",
+                    SmallImageKey = "smallimg",
+                    SmallImageText = "smallimage_text"
+                }
+            };
+            rpc.SetPresence(presence);
         }
     }
 }
